11f08d77b0debbc6d4227f9f432b335fe266ce74
Tom Sam <thayan.siva@gmail.com>
2022-06-08 19:40:41 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const puppeteer = require("puppeteer");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.get("/screenshot", async (req, res) => {
+try{
+  console.log("recieved");
+ const browser = await puppeteer.launch({
+  'args' : [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ]
+});
+  const page = await browser.newPage();
+  await page.goto(req.query.url); // URL is given by the "user" (your client-side application)
+
+  await page.evaluate(async () => {
+    // Scroll down to bottom of page to activate lazy loading images
+    document.body.scrollIntoView(false);
+
+    // Wait for all remaining lazy loading images to load
+    await Promise.all(
+      Array.from(document.getElementsByTagName("img"), (image) => {
+        if (image.complete) {
+          return;
+        }
+
+        return new Promise((resolve, reject) => {
+          image.addEventListener("load", resolve);
+          image.addEventListener("error", reject);
+        });
+      })
+    );
+  });
+  const logo = await page.$("body");
+  const screenshotBuffer = await logo.screenshot();
+
+  // Respond with the image
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+    "Content-Length": screenshotBuffer.length,
+  });
+  res.end(screenshotBuffer);
+
+  await browser.close();
+}
+catch(e){
+	res.status(422).send({"message":"Could not screenshot"})
+	
+}
+});
+
+app.listen(process.env.PORT || 5002);
